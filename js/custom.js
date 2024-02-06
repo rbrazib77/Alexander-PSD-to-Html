5e3226a2c33f2ab3a968a43d03549adbca1d2d97
rbrazib77 <rbrazib77@gmail.com>
2024-02-07 02:30:39 +0600

    Alexander project Upload GitHub

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+$(function(){
+    // Banner Section Slider Start
+    $('.slick_slider').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow:'<i class="fa fa-chevron-left prev"</i>',
+        nextArrow:'<i class="fa fa-chevron-right next"</i>',
+      });
+      // Banner Section Slider End
+
+
+});
